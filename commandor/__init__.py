cae6576697c45fde18795be21aa4a3f0f7c530ff
Ravin D <ravin.d3107@outlook.com>
2025-07-18 20:12:05 +0530

    Releasing v0.0.1 Beta

--- commandor/__init__.py
+++ commandor/__init__.py
@@ -2,7 +2,7 @@
 Commandor - An intelligent terminal assistant that uses AI to convert natural language to shell commands.
 """
 
-__version__ = "1.0.2"
+__version__ = "0.0.1"
 __author__ = "Ravin D"
 __email__ = "ravin.d3107@outlook.com"
 
